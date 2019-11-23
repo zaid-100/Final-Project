339003ea8c11cc6ea8b18a68283b7ecb50ab0d05
unknown <zaid.ahmed@centre.edu>
2019-11-22 21:45:00 -0500

    post 1st office hours

--- Code.js
+++ Code.js
@@ -1,25 +1,31 @@
 var AllPromise = d3.csv("GDP per Hour.csv");
 var hoursPromise = d3.csv("Hours Worked per Year.csv");
 var compPromise = d3.csv("Comp.csv");
-hoursPromise.then(
-function(countries)
-{  
-//    console.log("Working", countries)      
-})
-
-
-AllPromise.then(
+Promise.all([AllPromise,hoursPromise,compPromise]).then(
 function(countries)
 {
-//  console.log("works", countries)   
     
-    
-})
-compPromise.then(
-function(countries)
+console.log("Working", countries)      
+}
+)
+var join = function(countries)
 {
-    console.log("workeee", countries)
-})
+var hash = {}
+countries[0].forEach(function(GDP)
+        {
+            hash
+        }
+                    )
+
+
+
+
+
+
+
+
+}
+
 //Do the hash joins
 //build the layout
 
